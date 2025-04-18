4da6712c57d6df66fea9c2748143317888a4e098
daniel john jacob <danieljj1805@gmail.com>
2025-04-19 00:02:38 +0530

    added main file

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Name: Daniel John Jacob
+// PRN: 23070126027
+// Batch: AIML A2
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread started.");
+
+        
+}
